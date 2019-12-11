dc5715f55c312f785ce9c77f14cb4d02c3b96d29
laurin-d <laurin.doerre@lgs-hu.eu>
2019-12-11 14:33:06 +0100

    added more slides

--- js/impress.js
+++ js/impress.js
@@ -318,7 +318,7 @@
             console.log("ID: " + idx);
             try {
                 // Seitenanzahl
-                document.getElementsByClassName('page-nr')[idx].innerHTML = "" + idx + 1;
+                document.getElementsByClassName('page-nr')[idx].innerHTML = "" + idx;
             } catch (e) {
 
             }
